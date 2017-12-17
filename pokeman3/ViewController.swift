68e791f93624e75ae4602b46b32751074572a55c
Daniel <maxis1314@>
2017-12-17 22:34:30 +0800

    Initial Commit

---
+++ pokeman3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeman3
+//
+//  Created by Daniel on 17/12/17.
+//  Copyright © 2017年 daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
